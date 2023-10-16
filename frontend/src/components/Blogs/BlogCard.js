55418bb63641408b42ae6f73f1c40540824c0bf3
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-10-16 20:59:42 +0530

    added page for user blogs

--- frontend/src/components/Blogs/BlogCard.js
+++ frontend/src/components/Blogs/BlogCard.js
@@ -1,15 +1,24 @@
-import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
+import formatDateAndTime from "../../utils/DateTimeUtils";
 
 const BlogCard = ({ props }) => {
+    
 
   return (
-    <Card>
+    <Card style={{ width: "100%", marginBottom: "20px" }}>
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            {formatDateAndTime(new Date(props.creationDateTime))}
+          </Card.Text>
+        </div>
+
         <Card.Text>{props.textBody}</Card.Text>
-        <Button variant="primary">Edit</Button>
-        <Button variant="danger">Delete</Button>
+        <Button variant="primary" style={{ marginRight: "20px" }}>
+          Edit
+        </Button>
+        <Button variant="danger" onClick = {() => handleDeleteBlog(props._id)}>Delete</Button>
       </Card.Body>
     </Card>
   );
--- frontend/src/pages/MyBlogs.js
+++ frontend/src/pages/MyBlogs.js
@@ -1,10 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import BlogCard from "../components/Blogs/BlogCard";
 
 const MyBlogs = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const [myBlogs, setMyBlogs] = useState();
 
-export default MyBlogs
+  const token = localStorage.getItem("token");
+
+  // fetching my blogs on page load
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/blog/getUserBlogs`, {
+        headers: { "X-Acciojob": token },
+      })
+      .then((res) => {
+        setMyBlogs(res.data.data);
+        // console.log(myBlogs);
+      })
+      .catch((err) => alert(err));
+  }, [token]);
+
+  return (
+    <div style={{ padding: "3rem" }}>
+      <h1 style={{ textAlign: "center", marginBottom: "50px" }}>My Blogs</h1>
+      {myBlogs?.map((blog) => (
+        <BlogCard props={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default MyBlogs;
