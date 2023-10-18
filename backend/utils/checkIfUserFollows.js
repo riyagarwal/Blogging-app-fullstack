42f457988e89f5ae2e8a450419d57491c00aa119
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-10-18 17:45:12 +0530

    added a util file for follow module

---
+++ backend/utils/checkIfUserFollows.js
@@ -0,0 +1,17 @@
+const { ERR, TRUE, FALSE } = require("../constants");
+const { getFollowData } = require("../repository/follow.repository");
+
+const checkIfUserFollows = async (followingUserId, followerUserId) => {
+  const followData = await getFollowData(followingUserId, followerUserId);
+
+  // Different states of response
+  if (followData.err) {
+    return ERR;
+  } else if (followData.data !== null) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+};
+
+module.exports = { checkIfUserFollows };
