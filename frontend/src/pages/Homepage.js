aae38f5c704d03edeb56ba9d7c7a68f6ac37947e
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-10-23 13:42:09 +0530

    added code for homepage

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+import Header from "../components/common/Header";
+import axios from "axios";
+import BlogCard from "../components/Blogs/BlogCard";
 
-const Homepage = () => {
+function Homepage() {
+  const [homeBlogs, setHomeBlogs] = useState();
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/blog/homepage-blogs`, {
+        headers: {
+          "X-Acciojob": token,
+        },
+      })
+      .then((res) => {
+        setHomeBlogs(res.data.data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, [token]);
+
   return (
-    <div>Homepage</div>
-  )
+    <div>
+      <Header />
+      <div style={{ padding: "30px" }}>
+        {homeBlogs?.map((blog) => (
+          <BlogCard props={blog} homepage={true} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
